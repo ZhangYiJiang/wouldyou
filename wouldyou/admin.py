e9a4df97c6a4f3e0cdd753d888d49d5940a37636
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-08-19 14:31:32 +0800

    add models and fixtures

--- wouldyou/admin.py
+++ wouldyou/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from . import models
 
-# Register your models here.
+
+class ActionInline(GenericTabularInline):
+    model = models.Action
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    inlines = [
+        ActionInline,
+    ]
+
+
+# class CustomUserAdmin(UserAdmin):
+#     inlines = [
+#         ActionInline,
+#     ]
+
+admin.site.register(models.Action)
+admin.site.register(models.Verb)
+admin.site.register(models.Profile, ProfileAdmin)
+
+# admin.site.unregister(User)
+# admin.site.register(User, CustomUserAdmin)
--- wouldyou/models.py
+++ wouldyou/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.conf import settings
 
-# Create your models here.
+
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Verb(BaseModel):
+    verb = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.verb
+
+
+class Profile(BaseModel):
+    name = models.CharField(max_length=200)
+    image = models.ImageField()
+
+    def __str__(self):
+        return self.name
+
+
+class Action(BaseModel):
+    verb = models.ForeignKey(Verb)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    limit = models.Q(app_label='django.contrib.auth', model=settings.AUTH_USER_MODEL) | \
+            models.Q(app_label='wouldyou', model='Profile')
+
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE,
+        limit_choices_to=limit
+    )
+
+    object_id = models.PositiveIntegerField()
+    subject = GenericForeignKey()
+
+    def __str__(self):
+        return '{0!s} {0!s} {0!s}'.format(self.user, self.verb, self.subject)

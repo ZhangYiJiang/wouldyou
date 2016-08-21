1d9b58e8b9a46a5e37d86d93bc27e8a8973c93aa
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-08-21 18:25:33 +0800

    fix media URL for app in development

--- wouldyou/urls.py
+++ wouldyou/urls.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^welcome/$', views.OnboardView.as_view(), name='onboard'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

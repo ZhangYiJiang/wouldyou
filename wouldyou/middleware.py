b2bcd110ca1a2a0188aa599386f3be34e60a3653
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-08-22 19:46:10 +0800

    remove auth check for CSRF skip

--- wouldyou/middleware.py
+++ wouldyou/middleware.py
@@ -5,7 +5,8 @@ from django.conf import settings
 
 def fb_skip_security_middleware(get_response):
     def middleware(request):
-        if request.user.is_authenticated and 'signed_request' in request.POST:
+        # TODO: Double check this does not create a CSRF security hole
+        if 'signed_request' in request.POST:
             request.csrf_processing_done = True
         response = get_response(request)
         response.xframe_options_exempt = True

29e0c1f1acd1af9fe8d9701a7c73fb4ab035480f
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-08-23 16:22:27 +0800

    remove unused Facebook template tag file

--- wouldyou/templatetags/facebook.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag(takes_context=True)
-def profile(context, key):
-    social = context.request.user.social_auth.get(provider='facebook')
-    return social.extra_data.get(key, None)

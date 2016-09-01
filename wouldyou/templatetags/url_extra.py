39a9db5a20486af1fc06fd2282b17942f131109d
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-09-02 06:59:49 +0800

    update homepage og:url tag

--- wouldyou/templatetags/url_extra.py
+++ wouldyou/templatetags/url_extra.py
@@ -1,4 +1,5 @@
 from django import template
+from django.shortcuts import resolve_url
 from django.templatetags.static import StaticNode
 
 register = template.Library()
@@ -7,4 +8,10 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def static_absolute(context, path):
     url = StaticNode.handle_simple(path)
+    return context['request'].build_absolute_uri(url)
+
+
+@register.simple_tag(takes_context=True)
+def url_absolute(context, path):
+    url = resolve_url(path)
     return context['request'].build_absolute_uri(url)

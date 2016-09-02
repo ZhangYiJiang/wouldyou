1a6f5396b2a0cc8745bbf89e1150c0776aeb069e
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-09-02 22:39:25 +0800

    move FB event handler to fb.html

--- wouldyou/static/wouldyou/js/app.js
+++ wouldyou/static/wouldyou/js/app.js
@@ -362,15 +362,6 @@ $.ajaxSetup({
   window.LoadingButton = widget;
 })(jQuery);
 
-(function () {
-  FB.Event.subscribe('edge.create', function () {
-    // Make sure the alert doesn't show up again after the like button is clicked
-    Cookies.set('like_alert_dismissed', true, {
-      expires: 365,  // One year
-    });
-  });
-})();
-
 (function ($) {
   $('body').on('click', '.share-btn', function (evt) {
     evt.preventDefault();
